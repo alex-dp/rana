06d7d99a8a286af55fa544600e32330baa18ed24
depa <depasquale.a@tuta.io>
2016-06-29 14:43:49 +0200

    first commit

---
+++ helper.h
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define WRONG_INPUT_FILE -2
+#define WRONG_OPTION -1
+
+#define ARGUMENTS 0			//args inside () become args inside opening tag <>
+#define CONTENT 1			//just act based on what you find
+#define EXPECT_CONTENT 3	//end opening tag
+#define NEXT_TAG 4			//close current tag </>
+#define NEW_TAG 5			//open a new tag
+#define QUOTING_INGORE_QUOTES 6		//for just plain text
+#define COPY 7				//everything that's not part of the syntax basipally
+
+string version = "0.0.1", ext = "rn";
+
+bool req_arg (char a) {
+	char args[] = {'o', 'e'};
+
+	for (int i = 0; i < sizeof args; ++i)
+		if (a == args[i]) return true;
+
+	return false;
+}
+
+const char* help () {
+	return "Rana gives HTML a better syntax.\n\nusage:\nrana [options] <infile>\n\n-o <outfile>\n-e <out_extension>\n-v\tPrint version number and exit\n-h\tPrint this and exit\n";
+}
+
+vector<string> split(const std::string &text, char sep) {
+    vector<string> tokens;
+    size_t start = 0, end = 0;
+    while ((end = text.find(sep, start)) != string::npos) {
+        string temp = text.substr(start, end - start);
+        if (temp != "") tokens.push_back(temp);
+        start = end + 1;
+    }
+    string temp = text.substr(start);
+    if (temp != "") tokens.push_back(temp);
+    return tokens;
+}
+
+bool has_suffix(const string &str, const string &suffix) {
+    return str.size() >= suffix.size() &&
+           str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+bool is_markup(string in) {
+	string exts[4] = {"html", "xml", "htm", "xhtml"};
+	for (int i = 0; i < 4; ++i)
+		if (has_suffix(in, '.' + exts[i]))
+			return true;
+
+	return false;
+}
+
+bool is_escaped(vector<unsigned char> v, int p) {
+	if (v.at(p - 1) != '\\') return false;
+	else return is_escaped(v, p - 1);
+}
---
+++ main.cpp
@@ -0,0 +1,172 @@
+/*Rana: a better syntax to markup*/
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include "helper.h"
+
+using namespace std;
+
+string infile = "\0", outfile = "\0", o_ext = "\0", tag_name = "";
+int ignore = -1, mode = NEW_TAG;
+char argfor = '0';
+vector<string> tag_names;
+bool quoting = false;
+
+int main (int argc, char** argv){
+	for(int i = 1; i < argc; i++) {		//ignore program name
+
+		if (argv[i][0] != '-' && i != ignore) {		//if it's not an opt nor an arg then it's the infile
+			infile = argv[i];
+			ignore = -1;
+		}
+
+		else if (argfor != '0') {		//if it's an arg
+			switch (argfor) {			//(what is it for)
+				case 'o':
+					outfile = argv[i];
+					break;
+				case 'e':
+					o_ext = argv[i];
+					break;
+				default:
+					cout << help();
+					return WRONG_OPTION;
+			}
+
+			argfor = '0';				//then the next is not an arg
+		}
+
+		else {							//then it's an option
+
+			if (req_arg(argv[i][1])){	//if opt requires an arg
+				ignore = i + 1;			//the next is it
+				argfor = argv[i][1];	//(keep in mind what for)
+			} else {					//otherwise it's just an option
+
+				switch (argv[i][1]) {
+					case 'v':
+						cout << version << '\n';
+						return 0;
+
+					default:
+					case 'h':
+						cout << help();
+						return WRONG_OPTION;
+				}
+			}
+		}
+	}
+
+	if (infile == "\0") {		//if no input file is given
+		cout << help();
+		return 0;
+	}
+
+	if (outfile == "\0"){		//if no out is given
+		if (has_suffix(infile, '.' + ext))		//if it's a {productname} file
+			outfile = split(infile, '.')[0] + (o_ext == "\0" ? ".html" : ('.' + o_ext));
+
+		else if (is_markup(infile))				//if it's *ml
+			outfile = split(infile, '.')[0] + '.' + ext;
+
+		else {
+			cout << help();
+			return WRONG_INPUT_FILE;
+		}
+	}
+
+	//load input into a vector
+	std::vector<unsigned char> v;
+	if (FILE *fp = fopen(infile.c_str(), "r"))
+	{
+		char buf[1024];
+		while (size_t len = fread(buf, 1, sizeof(buf), fp))
+			v.insert(v.end(), buf, buf + len);
+		fclose(fp);
+	}
+
+	//open the output file, parse the input and write to output
+	const char *outfile_c = outfile.c_str();
+	if(FILE *out = fopen(outfile_c, "w")) {
+		for (int i = 0; i < v.size(); i++) {
+			switch (v.at(i)) {
+				case '(':
+					mode = ARGUMENTS;
+					break;
+				case '{':
+					mode = CONTENT;
+					break;
+				case '}':
+					mode = NEXT_TAG;
+					break;
+				case ')':
+					mode = EXPECT_CONTENT;
+					break;
+				case '"':
+					if (!is_escaped(v, i)) {
+						mode = mode == ARGUMENTS ?
+							ARGUMENTS : QUOTING_INGORE_QUOTES;
+					}
+					break;
+				case ' ':
+				case '\n':
+				case '\t':
+					if (mode != ARGUMENTS)
+						mode = COPY;
+					break;
+				default:		//if it does not start a new token
+					if (mode != ARGUMENTS)
+						mode = NEW_TAG;
+			}
+
+			switch (mode) {
+				case ARGUMENTS:
+					if (v.at(i) == '(' && !quoting)
+						fprintf(out, " ");
+					else fprintf(out, "%c", v.at(i));
+					if (v.at(i) == '"' && !is_escaped(v, i))
+						quoting = !quoting;
+					break;
+				case CONTENT:
+					mode = COPY;
+					break;
+				case NEXT_TAG:
+					fprintf(out, "%c%c%s%c", '<', '/', tag_names.at(tag_names.size() - 1).c_str(), '>');
+					tag_names.pop_back();
+					mode = COPY;
+					break;
+				case EXPECT_CONTENT:
+					fprintf(out, "%c", '>');
+					mode = COPY;
+					break;
+				case NEW_TAG:
+					tag_name = "";
+					try {
+						while(v.at(i) != '(') {
+							tag_name += v.at(i);
+							i++;
+						}
+					} catch (...) {}
+					tag_names.push_back(tag_name);
+					fprintf(out, "%c%s", '<', tag_name.c_str());
+					mode = COPY;
+					i--;
+					break;
+				case QUOTING_INGORE_QUOTES:
+					if (v.at(i) != '"')
+						fprintf(out, "%c", v.at(i));
+					else mode = COPY;
+					break;
+				case COPY:
+					fprintf(out, "%c", v.at(i));
+					break;
+			}
+		}
+		fprintf(out, "%c", '\n');
+		fclose(out);
+	}
+
+	return 0;
+}
